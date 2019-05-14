721d29d3e5f34c93844a4399b2a6abd15858f486
xnng <xnng77@gmail.com>
2019-05-14 16:49:02 +0800

    refact parse config

---
+++ config/parseConfig.js
@@ -0,0 +1,5 @@
+export default {
+  id: 'khYEI0xFyAnVCUpO',
+  key: 'iFPUdLm3yCPNnRVknBWHn4z5VDczgHOL',
+  url: 'http://47.95.200.31:1340/api/1'
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
+import parseConfig from '../../config/parseConfig';
 
-// var Parse = require('parse');
-const Parse = require('parse')
-Parse.initialize('khYEI0xFyAnVCUpO', 'iFPUdLm3yCPNnRVknBWHn4z5VDczgHOL')
-Parse.serverURL = 'http://47.95.200.31:1340/api/1'
-import TownMapList from '@/views/townMapList'
-import HomeMapList from '@/views/homeMapList'
-import AdvertisementList from '@/views/advertisementList.vue'
-import Test from '@/components/layout.vue'
+const Parse = require('parse');
+Parse.initialize(parseConfig.id, parseConfig.key);
+Parse.serverURL = parseConfig.url;
+import TownMapList from '@/views/townMapList';
+import HomeMapList from '@/views/homeMapList';
+import AdvertisementList from '@/views/advertisementList.vue';
+import Test from '@/components/layout.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
-Vue.prototype.$parse = Parse
+Vue.prototype.$parse = Parse;
 // var AV = require('leancloud-storage');
 
 export default new Router({
@@ -43,4 +43,4 @@ export default new Router({
       component: Test
     }
   ]
-})
+});
